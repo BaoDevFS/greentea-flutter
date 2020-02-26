f1056db1abf38471cf1930f7ceb81c6fc251d9f7
BaoNguyen <17130010@st.hcmuaf.edu.vn>
2020-02-26 19:54:13 +0700

    helo

--- lib/widget/scanPage.dart
+++ lib/widget/scanPage.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:greentea/define.dart';
 import 'package:greentea/model/delegate.dart';
@@ -30,6 +31,7 @@ class _ScanCodeState extends State<ScanCode> {
   String barcode = "", mesage;
   Delegate delegate;
   User user;
+
   _ScanCodeState({this.type, this.unit, this.token}) {
     print("this is constructor");
     print(token);
@@ -48,7 +50,7 @@ class _ScanCodeState extends State<ScanCode> {
     getInforDelegate(type);
   }
 
-  changePage(String type,bool _bool){
+  changePage(String type, bool _bool) {
     if (type == 'checkinManual') {
       if (_bool) {
 //        Navigator.push(
@@ -73,7 +75,7 @@ class _ScanCodeState extends State<ScanCode> {
 //          MaterialPageRoute(
 ////              builder:t
 //              ));
-    print("ckecintrue");
+      print("ckecintrue");
     } else {
 //      Navigator.push(
 //          context,
@@ -97,17 +99,20 @@ class _ScanCodeState extends State<ScanCode> {
         break;
     }
   }
+
   //checkin or checkout dua vao type truyen vao
-    checkin(String type) async {
+  checkin(String type) async {
     var uri;
     if (type == "checkin") {
-      print("ischeckIn"+barcode+"${unit.id}");
+      print("ischeckIn" + barcode + "${unit.id}");
 //      url = 'http://youth.gtnlu.site/api/checkin?data=$barcode&id=${unit.id}';
-      uri =Uri.http('youth.gtnlu.site', 'api/checkin',{'data':barcode,'id':'${unit.id}'});
+      uri = Uri.http('youth.gtnlu.site', 'api/checkin',
+          {'data': barcode, 'id': '${unit.id}'});
     } else {
-      print("ischeckOut"+barcode+"${unit.id}");
+      print("ischeckOut" + barcode + "${unit.id}");
 //      url = 'http://youth.gtnlu.site/api/checkout?data=$barcode&id=${unit.id}';
-      uri =Uri.http('youth.gtnlu.site', 'api/checkout',{'data':barcode,'id':'${unit.id}'});
+      uri = Uri.http('youth.gtnlu.site', 'api/checkout',
+          {'data': barcode, 'id': '${unit.id}'});
     }
     final response = await http
         .get(uri, headers: {HttpHeaders.authorizationHeader: "Bearer $token"});
@@ -117,52 +122,85 @@ class _ScanCodeState extends State<ScanCode> {
       if (body['status'] == 200) {
         delegate = Delegate.fromJson(body['member']);
         mesage = body['mes'];
-        print("mesage:"+mesage);
+        print("mesage:" + mesage);
         changePage(type, true);
       } else {
         mesage = body['mes'];
-        print("mesage:"+mesage);
+        print("mesage:" + mesage);
         changePage(type, false);
       }
     } else {
       mesage = "SERVER EROR";
-      print("mesage:"+mesage);
+      print("mesage:" + mesage);
       changePage(type, false);
     }
   }
 
-   checkinManual() async {
-     var uri = Uri.http('youth.gtnlu.site', '/api/program/checkin',{'id':'${unit.id}','idNlu':barcode});
+  checkinManual() async {
+    var uri = Uri.http('youth.gtnlu.site', '/api/program/checkin',
+        {'id': '${unit.id}', 'idNlu': barcode});
     final response = await http
         .get(uri, headers: {HttpHeaders.authorizationHeader: "Bearer $token"});
     if (response.statusCode == 200) {
       final body = json.decode(response.body);
       if (body['status'] == 200) {
-        user = User(iddep: body['user']['idp'],idNlu: body['user']['idNlu'],);
+        user = User(
+          iddep: body['user']['idp'],
+          idNlu: body['user']['idNlu'],
+        );
         mesage = body['mes'];
-        print("mesage:"+mesage);
+        print("mesage:" + mesage);
         changePage('checkinManual', true);
       } else {
         mesage = body['mes'];
-        print("mesage:"+mesage);
+        print("mesage:" + mesage);
         changePage('checkinManual', false);
       }
     } else {
       mesage = "SERVER EROR";
-      print("mesage:"+mesage);
+      print("mesage:" + mesage);
       changePage('checkinManual', false);
     }
   }
 
+//  GlobalKey<>
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     print("this is biuld");
     _scan();
     return Center(
-      child: CircularProgressIndicator(),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  shape: BoxShape.circle,
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  "Continue",
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  shape: BoxShape.circle,
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  "Back",
+                  style: TextStyle(fontSize: 15),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+          ]),
     );
   }
-
-
 }
